d70a0f485fc844fe82062c96ee75ab6f85f6305f
anythinks <alauddinazhary05@gmail.com>
2025-11-10 16:57:53 +0700

    fix error import button

--- src/app/components/theme-switcher.tsx
+++ src/app/components/theme-switcher.tsx
@@ -1,7 +1,7 @@
 // app/components/ThemeSwitcher.tsx
 "use client";
 
-import { Button } from "@heroui/button";
+import { Button } from "@heroui/react";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
 
